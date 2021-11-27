82e599c8a48c6d969cce093869ebaa70e8e229e1
Gaein nidb <mail@gaein.cn>
2021-11-27 18:35:49 +0800

    Update controller

--- DotBlog.Server/Controllers/ArticlesController.cs
+++ DotBlog.Server/Controllers/ArticlesController.cs
@@ -1,28 +1,26 @@
 ﻿using AutoMapper;
 using DotBlog.Server.Dto;
+using DotBlog.Server.Dto.QueryModel;
 using DotBlog.Server.Entities;
 using DotBlog.Server.Services;
+using DotBlog.Shared.Dto;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Text.Json;
 using System.Threading.Tasks;
-using DotBlog.Shared;
-using DotBlog.Shared.Dto;
 
 namespace DotBlog.Server.Controllers
 {
-    [Route(Startup.ApiVersion + "/[controller]")]
+    [Route("v1/[controller]")]
     [ApiController]
     public class ArticlesController : ControllerBase
     {
-        // 通过DI注入的只读服务
-
+        #region 只读字段
         /// <summary>
         /// 文章操作服务
         /// </summary>
-        private readonly IArticlesService _articleService;
+        private readonly IArticlesService _articlesService;
 
         /// <summary>
         /// 日志记录服务
@@ -33,45 +31,33 @@ namespace DotBlog.Server.Controllers
         /// 对象映射服务
         /// </summary>
         private readonly IMapper _mapper;
+        #endregion
 
-        // 自定义字段
-
-        /// <summary>
-        /// 格式化JSON
-        /// </summary>
-        private readonly JsonSerializerOptions _printOptions = new() { WriteIndented = true };
-
-        // 构造函数
-        public ArticlesController(IArticlesService articleService, ILogger<ArticlesController> logger, IMapper mapper)
+        #region 构造函数
+        public ArticlesController(IArticlesService articlesService, ILogger<ArticlesController> logger, IMapper mapper)
         {
             // 依赖注入
-            _articleService = articleService ??
-                              throw new ArgumentNullException(nameof(articleService));
-            _logger = logger ??
-                      throw new ArgumentNullException(nameof(logger));
-            _mapper = mapper ??
-                      throw new ArgumentNullException(nameof(mapper));
+            _articlesService = articlesService;
+            _logger = logger;
+            _mapper = mapper;
         }
+        #endregion
 
+        #region WebApi
+
+        #region 获取相关
         /// <summary>
         /// 获取文章列表
         /// </summary>
-        /// <param name="limit">限制数</param>
-        /// <returns>HTTP 200</returns>
+        /// <param name="page">页码</param>
+        /// <param name="count">数量</param>
+        /// <returns>文章集合</returns>
         [HttpGet]
-        public async Task<ActionResult<ICollection<ArticleListDto>>> GetArticleList([FromQuery] int? limit)
+        public async Task<ActionResult<IList<ArticleListDto>>> GetArticleList([FromQuery]ArticleGetDtoParameters? query)
         {
-            _logger.LogInformation($"Match method {nameof(GetArticleList)}.");
-            // 获取文章列表
-            var articlesList = await _articleService.GetAllAsync(limit);
-
-            // 判空
-            if (articlesList.Count == 0)
-            {
-                _logger.LogInformation("No articles were found, return a empty list.");
-            }
-
-            _logger.LogDebug($"Find articles: {JsonSerializer.Serialize(articlesList, _printOptions)}");
+            var articlesList = query is null
+                ? await _articlesService.GetAllAsync()
+                : await _articlesService.GetAllAsync(query);
 
             // 返回Dto结果
             return Ok(
@@ -82,24 +68,19 @@ namespace DotBlog.Server.Controllers
         /// <summary>
         /// 获取文章
         /// </summary>
-        /// <param name="articleId">文章ID</param>
-        /// <returns>HTTP 200/ HTTP 404 / HTTP 400</returns>
+        /// <param name="articleId">文章id</param>
+        /// <returns>文章实例</returns>
         [HttpGet("{articleId}", Name = nameof(GetArticle))]
-        public async Task<ActionResult<ArticleContentDto>> GetArticle([FromRoute] uint articleId)
+        public async Task<ActionResult<ArticleContentDto>> GetArticle(uint articleId)
         {
-            _logger.LogInformation($"Match method {nameof(GetArticle)}.");
-
             // 获取文章
-            var article = await _articleService.GetAsync(articleId);
+            var article = await _articlesService.GetAsync(articleId);
 
             // 判空
-            if (article == null)
+            if (article is null)
             {
-                _logger.LogInformation("No article was found, return a NotFound.");
                 return NotFound();
             }
-
-            _logger.LogDebug($"Find article: {JsonSerializer.Serialize(article, _printOptions)}");
 
             // 返回Dto结果
             return Ok(
@@ -107,55 +88,48 @@ namespace DotBlog.Server.Controllers
             );
         }
 
+        #endregion
+
+        #region 更新相关
 
         /// <summary>
         /// 更新文章
         /// </summary>
-        /// <param name="articleId">文章ID</param>
-        /// <param name="inputArticle">文章实例</param>
-        /// <returns>HTTP 200 / HTTP 404 / HTTP 400</returns>
-        //[Authorize]
-        [HttpPut("{articleId}")]
-        public async Task<ActionResult<ArticleContentDto>> UpdateArticle([FromRoute] uint articleId, [FromBody] ArticleUpdateDto inputArticle)
+        /// <param name="articleId">文章id</param>
+        /// <param name="patchDocument">Patch Document</param>
+        /// <returns></returns>
+        [HttpPatch("{articleId}")]
+        public async Task<IActionResult> UpdateArticle(
+            uint articleId, 
+            JsonPatchDocument<ArticleUpdateDto> patchDocument)
         {
-            _logger.LogInformation($"Match method {nameof(UpdateArticle)}.");
+            var article = await _articlesService.GetAsync(articleId);
 
-            // 获取旧文章
-            var articleOld = await _articleService.GetAsync(articleId);
-
-            if (articleOld == null)
-            {
-                _logger.LogInformation("No article was found, return a NotFound.");
+            if (article is null)
                 return NotFound();
-            }
 
-            // 更新文章
-            _mapper.Map(inputArticle, articleOld);
+            var dtoToPatch = _mapper.Map<ArticleUpdateDto>(article);
 
-            // 保存更改
-            if (!await _articleService.SaveChangesAsync())
-            {
-                _logger.LogError("Cannot save changes, UnKnow error.");
-            }
+            // TODO: 需要处理验证错误
+            patchDocument.ApplyTo(dtoToPatch);
 
-            // 返回Dto结果
-            return Ok(
-                _mapper.Map<ArticleContentDto>(articleOld)
-            );
+            _mapper.Map(dtoToPatch, article);
+
+            await _articlesService.SaveAsync();
+
+            return NoContent();
         }
 
         /// <summary>
         /// 更新文章点赞
         /// </summary>
         /// <param name="articleId">文章ID</param>
-        /// <returns>HTTP 204 / HTTP 404</returns>
+        /// <returns></returns>
         [HttpPut("{articleId}/Like")]
         public async Task<IActionResult> UpdateArticleLike([FromRoute] uint articleId)
         {
-            _logger.LogInformation($"Match method {nameof(UpdateArticleLike)}.");
-
             // 获取文章
-            var article = await _articleService.GetAsync(articleId);
+            var article = await _articlesService.GetAsync(articleId);
 
             // 判断是否找到文章
             // ReSharper disable once InvertIf
@@ -166,13 +140,9 @@ namespace DotBlog.Server.Controllers
             }
 
             // 更新点赞
-            _articleService.Like(article);
+            _articlesService.Like(article);
 
-            // ReSharper disable once InvertIf
-            if (!await _articleService.SaveChangesAsync())
-            {
-                _logger.LogError("Cannot save changes, UnKnow error.");
-            }
+            await _articlesService.SaveAsync();
 
             // 返回结果
             return NoContent();
@@ -186,10 +156,7 @@ namespace DotBlog.Server.Controllers
         [HttpPut("{articleId}/Read")]
         public async Task<IActionResult> UpdateArticleRead([FromRoute] uint articleId)
         {
-            _logger.LogInformation($"Match method {nameof(UpdateArticleRead)}.");
-
-            // 获取文章
-            var article = await _articleService.GetAsync(articleId);
+            var article = await _articlesService.GetAsync(articleId);
 
             // 判断是否找到文章
             // ReSharper disable once InvertIf
@@ -200,65 +167,58 @@ namespace DotBlog.Server.Controllers
             }
 
             // 更新文章阅读数
-            _articleService.Read(article);
+            _articlesService.Read(article);
 
             // ReSharper disable once InvertIf
-            if (!await _articleService.SaveChangesAsync())
-            {
-                _logger.LogError("Cannot save changes, UnKnow error.");
-            }
+            await _articlesService.SaveAsync();
 
             // 返回结果
             return NoContent();
         }
 
+        #endregion
+
+        #region 新建相关
+
         /// <summary>
         /// 新建文章
         /// </summary>
-        /// <param name="inputArticle">文章实例</param>
-        /// <returns>HTTP 201 / HTTP 202 / HTTP 400</returns>
-        //[Authorize]
+        /// <param name="articleDto">文章实例</param>
+        /// <returns></returns>
         [HttpPost]
-        public async Task<ActionResult<ArticleContentDto>> CreateArticle([FromBody] ArticleAddDto inputArticle)
+        public async Task<ActionResult<ArticleContentDto>> CreateArticle([FromBody] ArticleAddDto articleDto)
         {
-            _logger.LogInformation($"Match method {nameof(CreateArticle)}.");
-
-            // Dto映射为实体
-            var article = _mapper.Map<Article>(inputArticle);
+            var article = _mapper.Map<Article>(articleDto);
 
             // 新建文章
-            var returnArticle = _articleService.PostArticle(article);
+            var addedArticle = _articlesService.Add(article);
 
-            if (returnArticle == null)
+            if (addedArticle is null)
             {
                 return Accepted();
             }
 
-            if (!await _articleService.SaveChangesAsync())
-            {
-                _logger.LogError("Cannot save changes, UnKnow error.");
-            }
+            await _articlesService.SaveAsync();
 
             // 返回Dto结果
-            var returnArticleDto = _mapper.Map<ArticleContentDto>(returnArticle);
-            return CreatedAtRoute(nameof(GetArticle), new { articleId = returnArticleDto.ArticleId }, returnArticleDto);
+            var returnedArticleDto = _mapper.Map<ArticleContentDto>(addedArticle);
+            return CreatedAtRoute(nameof(GetArticle), new { articleId = returnedArticleDto.ArticleId }, returnedArticleDto);
         }
+
+        #endregion
+
+        #region 删除相关
 
         /// <summary>
         /// 删除文章
         /// </summary>
         /// <param name="articleId">文章ID</param>
         /// <returns>HTTP 204 / HTTP 404 / HTTP 400</returns>
-        //[Authorize]
         [HttpDelete("{articleId}")]
         public async Task<IActionResult> DeleteArticle([FromRoute] uint articleId)
         {
-            _logger.LogInformation($"Match method {nameof(DeleteArticle)}.");
-
-
-
             // 获取文章
-            var article = await _articleService.GetAsync(articleId);
+            var article = await _articlesService.GetAsync(articleId);
 
             // 判断是否找到文章
             // ReSharper disable once InvertIf
@@ -269,15 +229,15 @@ namespace DotBlog.Server.Controllers
             }
 
             // 删除文章
-            _articleService.DeleteArticle(article);
+            _articlesService.Remove(article);
 
-            if (!await _articleService.SaveChangesAsync())
-            {
-                _logger.LogError("Cannot save changes, UnKnow error.");
-            }
+            await _articlesService.SaveAsync();
 
             // 返回结果
             return NoContent();
         }
+        #endregion
+
+        #endregion
     }
 }
